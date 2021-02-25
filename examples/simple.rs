654398b87bab1c71003b443a37a29d6f287ffc08
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-02-25 17:11:24 +0100

    Minimal example for issue #20

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,14 +1,4 @@
+
 fn main() {
-    println!();
-    bunt::println!("I really like {$yellow}lemons{/$}! Like, {$blue+italic}a lot{/$}.");
-
-    let v = vec![1, 2, 3];
-    bunt::println!("Here is some data: {[green]:?}. {$bold}Length: {[cyan]}{/$}", v, v.len());
-    println!();
-
-    let ty = "u32";
-    bunt::eprintln!("{$bold+red}error:{/$} invalid value for type `{[blue]}`", ty);
-    bunt::eprintln!("");
-    bunt::eprintln!("{$italic}Just {$yellow}kidding{/$}{/$}, there is no {$magenta}error{/$} :)");
-    println!();
+    bunt::foo!(peter);
 }
--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,56 +1,6 @@
-//! These are the docs for the crate `bunt-macros`. This is just implementation
-//! detail, please see the crate `bunt` for the real docs.
+use proc_macro::TokenStream;
 
-use proc_macro::TokenStream as TokenStream1;
-use proc_macro2::TokenStream;
-
-#[macro_use]
-mod err;
-mod gen;
-mod ir;
-mod parse;
-
-#[cfg(test)]
-mod tests;
-
-use crate::{
-    err::Error,
-    ir::{Style, WriteInput},
-};
-
-
-// Docs are in the `bunt` reexport.
 #[proc_macro]
-pub fn style(input: TokenStream1) -> TokenStream1 {
-    run(input, |input| {
-        let style = Style::parse_from_tokens(input)?;
-        Ok(style.to_tokens())
-    })
-}
-
-// Docs are in the `bunt` reexport.
-#[proc_macro]
-pub fn write(input: TokenStream1) -> TokenStream1 {
-    run(input, |input| parse::parse(input, WriteInput::parse)?.gen_output())
-}
-
-// Docs are in the `bunt` reexport.
-#[proc_macro]
-pub fn writeln(input: TokenStream1) -> TokenStream1 {
-    run(input, |input| {
-        let mut input = parse::parse(input, WriteInput::parse)?;
-        input.format_str.add_newline();
-        input.gen_output()
-    })
-}
-
-/// Performs the conversion from and to `proc_macro::TokenStream` and converts
-/// `Error`s into `compile_error!` tokens.
-fn run(
-    input: TokenStream1,
-    f: impl FnOnce(TokenStream) -> Result<TokenStream, Error>,
-) -> TokenStream1 {
-    f(input.into())
-        .unwrap_or_else(|e| e.to_compile_error())
-        .into()
+pub fn writeln(input: TokenStream) -> TokenStream {
+    panic!("{:#?}", input);
 }
